2dd9cfff2b3d36dc554eb5ca1c13d5dec59d6289
Laura Wu <leemaiwu@gmail.com>
2023-05-23 10:07:59 -0600

    Completed with charat and ternary operator

---
+++ solution.js
@@ -0,0 +1,6 @@
+const areYouPlayingBanjo = name => {
+  return name.charAt(0) == 'R' || name.charAt(0) == 'r' ? `${name} plays banjo` : `${name} does not play banjo`
+}
+
+console.log(areYouPlayingBanjo('Ray'))
+console.log(areYouPlayingBanjo('Mike'))
